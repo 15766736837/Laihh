7bbf2dacd9c99539cff066f037f9dfe94d44a23d
janson <805464248@qq.com>
2023-06-28 00:53:32 +0800

    新增浮动按钮

--- app/src/main/java/com/example/androiddemo/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/androiddemo/ui/fragment/HomeFragment.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.androiddemo.R;
+import com.example.androiddemo.app.BaseApplication;
+import com.example.androiddemo.app.BaseFragment;
 
-public class HomeFragment extends Fragment {
-    @Nullable
+public class HomeFragment extends BaseFragment {
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected void initView() {
+        contentView.findViewById(R.id.addRoom).setOnClickListener(v -> {
+            // TODO: 2023/6/28 创建会议室
+            Toast.makeText(requireContext(), "创建会议室", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    protected void initContent(Bundle savedInstanceState) {
+
     }
 }
